8b24b0f5c60deb8c1a9f189c1e0b2eaf89cef6f0
Yurika Jacobsson <nakiruy@gmail.com>
2017-10-09 19:14:42 -0700

    Add initial App component.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  super(props);
+  this.state = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        This is the most simplest boilerplate yet. Oh, hey there.
+      </div>
+    )
+  }
+}
+
+export default App;
