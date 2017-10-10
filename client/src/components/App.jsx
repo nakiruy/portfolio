b4a1cde269aa4ae20e005ddcf3aa45df7f61a699
Yurika Jacobsson <nakiruy@gmail.com>
2017-10-09 20:06:04 -0700

    Edit message on App component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -11,7 +11,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        This is the most simplest boilerplate yet. Oh, hey there.
+        <h3>This is a React Boilerplate.</h3>
+        <h4>Feel free to add whatever you like :)</h4>
       </div>
     )
   }
