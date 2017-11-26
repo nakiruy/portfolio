0f06bf45c6e6310542b3b0a2bba49fc305293573
Yurika Jacobsson <nakiruy@gmail.com>
2017-11-25 16:33:31 -0800

    Move styles to client folder instead of src.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -14,13 +14,13 @@ class Contact extends React.Component {
       <div id="contact">
         <h5>CONTACT</h5>
         <a href="https://www.instagram.com/yurikaffeine/">
-          <img class="image" src="./assets/instagram.png" alt="Instagram" /></a>
+          <img className="image" src="./assets/instagram.png" alt="Instagram" /></a>
         <a href="https://github.com/nakiruy">
-          <img class="image" src="./assets/github.png" alt="GitHub" /></a>
+          <img className="image" src="./assets/github.png" alt="GitHub" /></a>
         <a href="https://www.linkedin.com/in/yurikajacobsson/">
-          <img class="image" src="./assets/linkedin.png" alt="LinkedIn" /></a>
+          <img className="image" src="./assets/linkedin.png" alt="LinkedIn" /></a>
         <a href="mailto:nakiruy@gmail.com">
-          <img class="image" src="./assets/email.png" alt="Email" /></a>
+          <img className="image" src="./assets/email.png" alt="Email" /></a>
       </div>
     )
   }
