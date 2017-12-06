201ea444517d19131f11ae35eadef68404b15f57
Yurika Jacobsson <nakiruy@gmail.com>
2017-12-05 17:20:33 -0800

    Add project images in assets.

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -16,9 +16,15 @@ class Portfolio extends React.Component {
         <div className="rectangle" id="bg-portfolio">
           <h5 id="portfolio-title">PORTFOLIO</h5>
           <div className="portfolio-img">
-            <div className="square" id="proj1"></div>
-            <div className="square" id="proj2"></div>
-            <div className="square" id="proj3"></div>
+            <div className="square" id="proj1" >
+              <img src="src=./assets/buzz.png" alt=""/>
+            </div>
+            <div className="square" id="proj2">
+              <img src="src=./assets/face2face.png" alt=""/>
+            </div>
+            <div className="square" id="proj3">
+              <img src="src=./assets/3scan.png" alt=""/>
+            </div>
           </div>
         </div>
       </div>
