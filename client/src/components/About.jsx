6d4400f76eba5ee7b21bcbfa270cd87e039458e0
Yurika Jacobsson <nakiruy@gmail.com>
2017-12-06 16:03:20 -0800

    client/src/components/About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -17,11 +17,11 @@ class About extends React.Component {
           <h5 id="about-title">ABOUT</h5>
           <div className="about-description">
             <h2>Hi there! I'm Yurika.</h2>
-            <h3>I love creating applications that make a positive impact on people and their daily lives.</h3>
-            <h3>With my background in Cognitive Science and experience in building full-stack applications,</h3>
-            <h3>I hope to integrate users with technology by utilizing my academic background and bridge them through my technical abilities.</h3>
-            <h3>I would love to join a team of engineers where we can tackle technical challenges and create user experiences that are both intuitive as well as efficient in its implementation.</h3>
-            <h3>Please touch base with me if you know of such a team!</h3>
+            <h2>I love creating applications that make a positive impact on people and their daily lives.</h2>
+            <h2>With my background in Cognitive Science and experience in building full-stack applications,</h2>
+            <h2>I hope to integrate users with technology by utilizing my academic background and bridge them through my technical abilities.</h2>
+            <h2>I would love to join a team of engineers where we can tackle technical challenges and create user experiences that are both intuitive as well as efficient in its implementation.</h2>
+            <h2>Please touch base with me if you know of such a team!</h2>
             </div>
         </div>
       </div>
