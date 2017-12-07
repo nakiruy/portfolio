6af410e6b09b3d6a3c9ec0b14edf9522460a01b1
Yurika Jacobsson <nakiruy@gmail.com>
2017-12-06 16:09:18 -0800

    Add styling changes on projects and contact buttons to work on mobile.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -17,11 +17,11 @@ class About extends React.Component {
           <h5 id="about-title">ABOUT</h5>
           <div className="about-description">
             <h2>Hi there! I'm Yurika.</h2>
-            <h2>I love creating applications that make a positive impact on people and their daily lives.</h2>
-            <h2>With my background in Cognitive Science and experience in building full-stack applications,</h2>
-            <h2>I hope to integrate users with technology by utilizing my academic background and bridge them through my technical abilities.</h2>
-            <h2>I would love to join a team of engineers where we can tackle technical challenges and create user experiences that are both intuitive as well as efficient in its implementation.</h2>
-            <h2>Please touch base with me if you know of such a team!</h2>
+            <h2>I'm a UI/UX Designer & Front-End Developer located in San Francisco Bay Area.</h2>
+            <h2>I love creating applications that are thoughtful in design and efficient in its implementation.</h2>
+            <h2>With my background in Cognitive Science from UC Berkeley and experience building applications,</h2>
+            <h2>My goal is to craft intuitive user experiences for clients and be the bridge between designers and engineers.</h2>
+            <h2>In my spare time, I obsess over sneakers (Adidas ftw!) and perfecting the science of making cold brew.</h2>
             </div>
         </div>
       </div>
