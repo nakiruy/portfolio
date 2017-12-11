dbf96b51a497a5f1b7199832572487cf84187ca5
Yurika Jacobsson <nakiruy@gmail.com>
2017-12-11 15:47:32 -0800

    Add shortcut icon for desktop and style redesign page.

--- client/src/components/ThreeScanRedesign.jsx
+++ client/src/components/ThreeScanRedesign.jsx
@@ -16,15 +16,17 @@ class ThreeScanRedesign extends React.Component {
     return (
       <div>
         <NavBar />
-        <div className="project-title">
-          <h1>3Scan Redesign</h1>
-          <h2>Overview</h2>
-            <h3>3Scan is a digital pathology company that offers to </h3>
-          <h2>Problems</h2>
-          <h2>Research</h2>
-          <h2>Process</h2>
-          <h2>Solution</h2>
-          <h2>Other Considerations</h2>
+        <div className="content">
+          <div className="project-description">
+            <h1 className="project-title">3Scan Redesign</h1>
+            <h2>Overview</h2>
+              <h3>3Scan is a digital pathology company that offers to </h3>
+            <h2>Problems</h2>
+            <h2>Research</h2>
+            <h2>Process</h2>
+            <h2>Solution</h2>
+            <h2>Other Considerations</h2>
+          </div>
         </div>
       </div>
     )
