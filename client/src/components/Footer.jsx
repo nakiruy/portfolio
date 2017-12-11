10f1dafbf52f8fabf9134a721382b068b434d375
Yurika Jacobsson <nakiruy@gmail.com>
2017-12-11 14:21:22 -0800

    Add hash router and a link to the redesign.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <h6 id="copyright">Made with 💕 by Yurika</h6>
+        <h6 id="copyright">Made with 💕 & ☕ by Yurika</h6>
         <h6 id="credit">Icons by Dave Gandy @ Flat Icon</h6>
       </div>
     )
